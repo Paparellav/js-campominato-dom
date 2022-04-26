2efc4ba13a9d8c6e8fd8575d384f170a8d1e0131
Paparellav <vitantoniopaparella@gmail.com>
2022-04-26 15:24:09 +0200

    bombe posizionate

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+//1. Al click del bottone Play deve partire il gioco [*];
+//2. Nascondere la scritta con la classe hidden [*];
+//3. Far vedere il container-grid rimuovendo la classe hidden [*];
+//4. Prelevare la scelta difficoltà dell'utente, salvare il numero delle celle all'interno di una variabile gridBoxes [*];
+//5. Generare 16 numeri casuali non ripetuti compresi tra 1 e gridBoxes [];
+//6. Generare le celle da 1 a gridBoxes in base alla difficoltà. Per ogni cella:
+    // - creare l'elemento HTML [*];
+    // - aggiungere la classe grid-box [*];
+    // - inserire lo span con il numero corrispondente [*];
+    // - appendere la cella generata al container [*];
+
+//7. Al click di una delle celle []:
+    // Se il numero della cella è presente nell'array delle bombe:
+        // La cella si colora di rosso
+        // Stampare il punteggio (numero di tentativi corretti)
+        // Fine gioco
+    //Altrimenti:
+        // La cella si colora di azzurro
+        // Il numero della cella viene salvato nell'array di numeri corretti
+        // Se la lunghezza dell'array di numeri corretti è uguale ai tentativi massimi:
+            // L'utente ha vinto
+
 const playButton = document.querySelector("button");
 playButton.addEventListener("click", function () {
 
@@ -18,6 +40,12 @@ playButton.addEventListener("click", function () {
     } else if (difficults === "crazy") {
         gridBoxes = 49;
     }
+
+    const bombsNumber = 16;
+    const bombsArray = generateRndNumbers(bombsNumber, gridBoxes);
+    const checkedCells = [];
+    console.log(bombsArray);
+    console.log(checkedCells);
 
     for (let i = 1; i <= gridBoxes; i++) {
 
@@ -42,10 +70,44 @@ playButton.addEventListener("click", function () {
         // aggiungo all'elmento appena creato la gestione del click
         
         newElement.addEventListener("click", function() {
-            this.classList.toggle("active");
+
+            if (bombsArray.includes(gridNumber)) {
+                this.classList.add("bomb-box");
+                gridContainer.style.pointerEvents = "none";
+                alert ("Hai perso!")
+            } else {
+                this.classList.add("active");
+                this.style.pointerEvents = "none";
+                checkedCells.push(gridNumber);
+            }
+            
         });
     
         // appendo questo elemnto al contenitore
         gridContainer.append(newElement);  
     }
 });
+
+
+// FUNCTIONS
+/**
+ * Description -> Generare numeri random non ripetuti
+ * @param {any} numberOfBombs -> Quantità di numeri random da generare (bombsNumber)
+ * @param {any} numbersRange -> Range di numeri da generare (da 1 a gridBoxes)
+ * @returns {any} -> Array di bombe
+ */
+function generateRndNumbers (numberOfBombs, numbersRange) {
+    const bombsArray = [];
+    while (bombsArray.length < 16) {
+        const randomNumber = getRndInteger(numberOfBombs, numbersRange);
+        if (!bombsArray.includes(randomNumber)) {
+            bombsArray.push(randomNumber);
+        }
+    }
+    
+    return bombsArray;
+}
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
